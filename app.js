49e1dc9f0aad896c78c96da1931caf28a8e01b59
captainAyan <ayanchakraborty105@gmail.com>
2022-04-13 00:20:57 +0530

    fix: :bug: fixed github page

--- app.js
+++ app.js
@@ -15,7 +15,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "script-src": ["'self'", "https://captainayan.github.io/"],
+      "style-src": null,
+    },
+  })
+);
+
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+// app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
 
 app.get("/api", (req, res) => {
   const width = parseInt(req.query.width) || 20;
